3ee3d4d259984ed6a4cdb3c217d804e20703ea73
841723 <841723@unizar.es>
2024-03-01 18:17:07 +0100

    Refactor

--- api/Node.js
+++ api/Node.js
@@ -6,14 +6,7 @@ const { Pool } = require('pg');
 const app = express();
 const port = 3000;
 
-// PostgreSQL credentials
-const pool = new Pool({
-  host: 'buy8mj63a08xmrni7uy4-postgresql.services.clever-cloud.com',
-  database: 'buy8mj63a08xmrni7uy4',
-  user: 'ufjuuu4tmgx8hdaosrpx',
-  password: 'q1kiKLYfUCeC2TMzERwubKYLdZ5rSu',
-  port: 50013
-});
+
 
 // Test the database connection
 pool.connect((err, client, done) => {
