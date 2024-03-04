22214df938fc318f80e563f307e24e88807e33d6
luoolmos <146450799+luoolmos@users.noreply.github.com>
2024-03-04 16:30:35 +0100

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -1,10 +1,41 @@
+-- Type: jugadorUDT
+
+-- DROP TYPE IF EXISTS grace_hopper."jugadorUDT";
+
+CREATE TYPE grace_hopper."jugadorUDT" AS
+(
+	"userName" character varying(16),
+	ficha character varying(16),
+	partida_actual integer,
+	sospechas character varying(4096),
+	posicion grace_hopper.posicion,
+	estado character(1)
+);
+
+ALTER TYPE grace_hopper."jugadorUDT"
+    OWNER TO ufjuuu4tmgx8hdaosrpx;
+
+
+-- Type: posicion
+
+-- DROP TYPE IF EXISTS grace_hopper.posicion;
+
+CREATE TYPE grace_hopper.posicion AS
+(
+	x integer,
+	y integer
+);
+
+ALTER TYPE grace_hopper.posicion
+    OWNER TO ufjuuu4tmgx8hdaosrpx;
+
 -- Table: grace_hopper.arma
 
 -- DROP TABLE IF EXISTS grace_hopper.arma;
 
 CREATE TABLE IF NOT EXISTS grace_hopper.arma
 (
-    nombre character varying(16)[] COLLATE pg_catalog."default" NOT NULL,
+    nombre character varying(16) COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT arma_pkey PRIMARY KEY (nombre)
 )
 
@@ -19,13 +50,14 @@ ALTER TABLE IF EXISTS grace_hopper.arma
 
 CREATE TABLE IF NOT EXISTS grace_hopper.bot
 (
-    "UserName" character varying(16)[] COLLATE pg_catalog."default" NOT NULL,
-    "nivelDificultad" integer DEFAULT 0,
-    CONSTRAINT bot_pkey PRIMARY KEY ("UserName"),
-    CONSTRAINT "userName" FOREIGN KEY ("UserName")
+    "userName" character varying(16) COLLATE pg_catalog."default" NOT NULL,
+    nivel_dificultad integer NOT NULL,
+    CONSTRAINT bot_pkey PRIMARY KEY ("userName"),
+    CONSTRAINT "bot_userName" FOREIGN KEY ("userName")
         REFERENCES grace_hopper.jugador ("userName") MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
+        NOT VALID
 )
 
 TABLESPACE pg_default;
@@ -39,17 +71,19 @@ ALTER TABLE IF EXISTS grace_hopper.bot
 
 CREATE TABLE IF NOT EXISTS grace_hopper.cartas_armas_jugador
 (
-    "userName" character varying(16)[] COLLATE pg_catalog."default" NOT NULL,
-    "cartaArma" character varying(16)[] COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT cartas_armas_jugador_pkey PRIMARY KEY ("userName", "cartaArma"),
-    CONSTRAINT "cartaArma" FOREIGN KEY ("cartaArma")
+    "userName" character varying(16) COLLATE pg_catalog."default" NOT NULL,
+    "cartasArma" character varying(16) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT cartas_armas_jugador_pkey PRIMARY KEY ("userName", "cartasArma"),
+    CONSTRAINT carta FOREIGN KEY ("cartasArma")
         REFERENCES grace_hopper.arma (nombre) MATCH SIMPLE
         ON UPDATE CASCADE
-        ON DELETE CASCADE,
-    CONSTRAINT "userName" FOREIGN KEY ("userName")
+        ON DELETE CASCADE
+        NOT VALID,
+    CONSTRAINT "user" FOREIGN KEY ("userName")
         REFERENCES grace_hopper.jugador ("userName") MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE
+        NOT VALID
 )
 
 TABLESPACE pg_default;
@@ -57,23 +91,16 @@ TABLESPACE pg_default;
 ALTER TABLE IF EXISTS grace_hopper.cartas_armas_jugador
     OWNER to ufjuuu4tmgx8hdaosrpx;
 
+
 -- Table: grace_hopper.cartas_lugar_jugador
 
 -- DROP TABLE IF EXISTS grace_hopper.cartas_lugar_jugador;
 
 CREATE TABLE IF NOT EXISTS grace_hopper.cartas_lugar_jugador
 (
-    "userName" character varying(16)[] COLLATE pg_catalog."default" NOT NULL,
-    "cartaLugar" character varying(16)[] COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT cartas_lugar_jugador_pkey PRIMARY KEY ("userName", "cartaLugar"),
-    CONSTRAINT "cartaLugar" FOREIGN KEY ("cartaLugar")
-        REFERENCES grace_hopper.lugar (nombre) MATCH SIMPLE
-        ON UPDATE CASCADE
-        ON DELETE CASCADE,
-    CONSTRAINT "userName" FOREIGN KEY ("userName")
-        REFERENCES grace_hopper.jugador ("userName") MATCH SIMPLE
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
+    "userName" character varying(16) COLLATE pg_catalog."default" NOT NULL,
+    "cartaLugar" character varying(16) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT cartas_lugar_jugador_pkey PRIMARY KEY ("userName", "cartaLugar")
 )
 
 TABLESPACE pg_default;
@@ -88,17 +115,19 @@ ALTER TABLE IF EXISTS grace_hopper.cartas_lugar_jugador
 
 CREATE TABLE IF NOT EXISTS grace_hopper.cartas_personajes_jugador
 (
-    "userName" character varying(16)[] COLLATE pg_catalog."default" NOT NULL,
-    "cartaPersonaje" character varying(16)[] COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT cartas_personajes_jugador_pkey PRIMARY KEY ("userName", "cartaPersonaje"),
-    CONSTRAINT "cartaPersonaje" FOREIGN KEY ("cartaPersonaje")
+    "userName" character varying(16) COLLATE pg_catalog."default" NOT NULL,
+    "cartaPersonaje" character varying(16) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT cartas_personajes_jugador_pkey PRIMARY KEY ("cartaPersonaje", "userName"),
+    CONSTRAINT carta FOREIGN KEY ("cartaPersonaje")
         REFERENCES grace_hopper.personajes (nombre) MATCH SIMPLE
         ON UPDATE CASCADE
-        ON DELETE SET NULL,
-    CONSTRAINT "userName" FOREIGN KEY ("userName")
+        ON DELETE CASCADE
+        NOT VALID,
+    CONSTRAINT "user" FOREIGN KEY ("userName")
         REFERENCES grace_hopper.jugador ("userName") MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE
+        NOT VALID
 )
 
 TABLESPACE pg_default;
@@ -106,26 +135,30 @@ TABLESPACE pg_default;
 ALTER TABLE IF EXISTS grace_hopper.cartas_personajes_jugador
     OWNER to ufjuuu4tmgx8hdaosrpx;
 
+
 -- Table: grace_hopper.conversacion
 
 -- DROP TABLE IF EXISTS grace_hopper.conversacion;
 
 CREATE TABLE IF NOT EXISTS grace_hopper.conversacion
 (
-    instante timestamp with time zone NOT NULL,
+    instante timestamp(6) with time zone NOT NULL,
+    "isQuestion" character(1) COLLATE pg_catalog."default",
     partida integer NOT NULL,
-    emisor character varying(16)[] COLLATE pg_catalog."default" NOT NULL,
-    contenido character varying(256)[] COLLATE pg_catalog."default" NOT NULL,
-    "isQuestion" character(1) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT conversacion_pkey PRIMARY KEY (instante, partida, emisor),
+    contenido character varying(4096) COLLATE pg_catalog."default" NOT NULL,
+    emisor character varying(16) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT conversacion_pkey PRIMARY KEY (partida, emisor, instante),
     CONSTRAINT emisor FOREIGN KEY (emisor)
-        REFERENCES grace_hopper.usuario ("userName") MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
+        REFERENCES grace_hopper.jugador ("userName") MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE SET NULL
+        NOT VALID,
     CONSTRAINT partida FOREIGN KEY (partida)
         REFERENCES grace_hopper.partida (id_partida) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE
+        NOT VALID,
+    CONSTRAINT "isQuestion Valido" CHECK ("isQuestion" = '0'::bpchar OR "isQuestion" = '1'::bpchar) NOT VALID
 )
 
 TABLESPACE pg_default;
@@ -133,26 +166,32 @@ TABLESPACE pg_default;
 ALTER TABLE IF EXISTS grace_hopper.conversacion
     OWNER to ufjuuu4tmgx8hdaosrpx;
 
+
 -- Table: grace_hopper.jugador
 
 -- DROP TABLE IF EXISTS grace_hopper.jugador;
 
 CREATE TABLE IF NOT EXISTS grace_hopper.jugador
+    OF grace_hopper."jugadorUDT"
 (
-    "userName" character varying(16)[] COLLATE pg_catalog."default" NOT NULL,
-    ficha character varying(16)[] COLLATE pg_catalog."default",
-    "partidaActual" integer,
-    sospechas character varying(4096)[] COLLATE pg_catalog."default",
-    "posicionTablero" character varying(8)[] COLLATE pg_catalog."default",
+    -- Inherited from type grace_hopper."jugadorUDT": "userName" character varying(16) COLLATE pg_catalog."default" NOT NULL,
+    -- Inherited from type grace_hopper."jugadorUDT": ficha character varying(16) COLLATE pg_catalog."default",
+    -- Inherited from type grace_hopper."jugadorUDT": partida_actual integer,
+    -- Inherited from type grace_hopper."jugadorUDT": sospechas character varying(4096) COLLATE pg_catalog."default" NOT NULL,
+    -- Inherited from type grace_hopper."jugadorUDT": posicion grace_hopper.posicion NOT NULL,
+    -- Inherited from type grace_hopper."jugadorUDT": estado character(1) COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT jugador_pkey PRIMARY KEY ("userName"),
     CONSTRAINT ficha FOREIGN KEY (ficha)
         REFERENCES grace_hopper.personajes (nombre) MATCH SIMPLE
-        ON UPDATE CASCADE
-        ON DELETE CASCADE,
-    CONSTRAINT "partidaActual" FOREIGN KEY ("partidaActual")
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID,
+    CONSTRAINT partida_actual FOREIGN KEY (partida_actual)
         REFERENCES grace_hopper.partida (id_partida) MATCH SIMPLE
         ON UPDATE CASCADE
-        ON DELETE CASCADE
+        ON DELETE SET NULL
+        NOT VALID,
+    CONSTRAINT "estado valido" CHECK (estado = '0'::bpchar OR estado = '1'::bpchar) NOT VALID
 )
 
 TABLESPACE pg_default;
@@ -160,13 +199,14 @@ TABLESPACE pg_default;
 ALTER TABLE IF EXISTS grace_hopper.jugador
     OWNER to ufjuuu4tmgx8hdaosrpx;
 
+
 -- Table: grace_hopper.lugar
 
 -- DROP TABLE IF EXISTS grace_hopper.lugar;
 
 CREATE TABLE IF NOT EXISTS grace_hopper.lugar
 (
-    nombre character varying(16)[] COLLATE pg_catalog."default" NOT NULL,
+    nombre character varying(16) COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT lugar_pkey PRIMARY KEY (nombre)
 )
 
@@ -175,43 +215,59 @@ TABLESPACE pg_default;
 ALTER TABLE IF EXISTS grace_hopper.lugar
     OWNER to ufjuuu4tmgx8hdaosrpx;
 
+
+-- Table: grace_hopper.mensajes_predefinidos
+
+-- DROP TABLE IF EXISTS grace_hopper.mensajes_predefinidos;
+
+CREATE TABLE IF NOT EXISTS grace_hopper.mensajes_predefinidos
+(
+    text character varying(64) COLLATE pg_catalog."default" NOT NULL,
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 0 MINVALUE 0 MAXVALUE 2147483647 CACHE 1 ),
+    CONSTRAINT mensajes_predefinidos_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS grace_hopper.mensajes_predefinidos
+    OWNER to ufjuuu4tmgx8hdaosrpx;
+
+
 -- Table: grace_hopper.partida
 
 -- DROP TABLE IF EXISTS grace_hopper.partida;
 
 CREATE TABLE IF NOT EXISTS grace_hopper.partida
 (
-    id_partida integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 3 START 1000 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    estado character varying(1) COLLATE pg_catalog."default",
-    fecha_ini date,
-    fecha_fin date,
-    tipo character varying(1) COLLATE pg_catalog."default",
-    turno character varying(16)[] COLLATE pg_catalog."default",
-    asesino character varying(16)[] COLLATE pg_catalog."default",
-    arma character varying(16)[] COLLATE pg_catalog."default",
-    lugar character varying(16)[] COLLATE pg_catalog."default",
+    id_partida integer NOT NULL,
+    estado character varying(1) COLLATE pg_catalog."default" NOT NULL,
+    fecha_ini date NOT NULL,
+    fecha_fin date NOT NULL,
+    tipo character varying(1) COLLATE pg_catalog."default" NOT NULL,
+    turno character varying(16) COLLATE pg_catalog."default",
+    asesino character varying(16) COLLATE pg_catalog."default",
+    arma character varying(16) COLLATE pg_catalog."default",
+    lugar character varying(16) COLLATE pg_catalog."default",
     CONSTRAINT partida_pkey PRIMARY KEY (id_partida),
     CONSTRAINT arma FOREIGN KEY (arma)
         REFERENCES grace_hopper.arma (nombre) MATCH SIMPLE
         ON UPDATE CASCADE
-        ON DELETE CASCADE
-        NOT VALID,
+        ON DELETE SET NULL,
     CONSTRAINT asesino FOREIGN KEY (asesino)
         REFERENCES grace_hopper.personajes (nombre) MATCH SIMPLE
         ON UPDATE CASCADE
-        ON DELETE CASCADE
-        NOT VALID,
+        ON DELETE SET NULL,
     CONSTRAINT lugar FOREIGN KEY (lugar)
         REFERENCES grace_hopper.lugar (nombre) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID,
+        ON UPDATE CASCADE
+        ON DELETE SET NULL,
     CONSTRAINT turno FOREIGN KEY (turno)
         REFERENCES grace_hopper.jugador ("userName") MATCH SIMPLE
         ON UPDATE CASCADE
-        ON DELETE SET NULL
-        NOT VALID,
-    CONSTRAINT "fechaFinMayor" CHECK (fecha_fin > fecha_ini) NOT VALID
+        ON DELETE CASCADE,
+    CONSTRAINT "estado partida" CHECK (estado::text = 'p'::text OR estado::text = '1'::text OR estado::text = '0'::text) NOT VALID,
+    CONSTRAINT "fecha correcta" CHECK (fecha_ini <= fecha_fin) NOT VALID,
+    CONSTRAINT tipo CHECK (tipo::text = 'o'::text OR tipo::text = 'l'::text) NOT VALID
 )
 
 TABLESPACE pg_default;
@@ -219,23 +275,13 @@ TABLESPACE pg_default;
 ALTER TABLE IF EXISTS grace_hopper.partida
     OWNER to ufjuuu4tmgx8hdaosrpx;
 
--- Trigger: verificar_turno
-
--- DROP TRIGGER IF EXISTS verificar_turno ON grace_hopper.partida;
-
-CREATE OR REPLACE TRIGGER verificar_turno
-    BEFORE INSERT OR UPDATE 
-    ON grace_hopper.partida
-    FOR EACH ROW
-    EXECUTE FUNCTION grace_hopper.validar_turno();
-
 -- Table: grace_hopper.personajes
 
 -- DROP TABLE IF EXISTS grace_hopper.personajes;
 
 CREATE TABLE IF NOT EXISTS grace_hopper.personajes
 (
-    nombre character varying(16)[] COLLATE pg_catalog."default" NOT NULL,
+    nombre character varying(16) COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT personajes_pkey PRIMARY KEY (nombre)
 )
 
@@ -244,19 +290,26 @@ TABLESPACE pg_default;
 ALTER TABLE IF EXISTS grace_hopper.personajes
     OWNER to ufjuuu4tmgx8hdaosrpx;
 
-    -- Table: grace_hopper.usuario
+
+-- Table: grace_hopper.usuario
 
 -- DROP TABLE IF EXISTS grace_hopper.usuario;
 
 CREATE TABLE IF NOT EXISTS grace_hopper.usuario
 (
-    "userName" character varying(16)[] COLLATE pg_catalog."default" NOT NULL,
-    passwd character varying(32)[] COLLATE pg_catalog."default" NOT NULL,
-    n_jugadas integer NOT NULL DEFAULT 0,
-    n_ganadas_local integer NOT NULL DEFAULT 0,
-    n_ganadas_online integer NOT NULL DEFAULT 0,
+    "userName" character varying(16) COLLATE pg_catalog."default" NOT NULL,
+    passwd character varying COLLATE pg_catalog."default" NOT NULL,
     "XP" integer NOT NULL DEFAULT 0,
-    CONSTRAINT usuario_pkey PRIMARY KEY ("userName")
+    n_ganadas_online integer NOT NULL DEFAULT 0,
+    n_ganadas_local integer NOT NULL DEFAULT 0,
+    n_jugadas integer NOT NULL DEFAULT 0,
+    CONSTRAINT usuario_pkey PRIMARY KEY ("userName"),
+    CONSTRAINT "user_userName" FOREIGN KEY ("userName")
+        REFERENCES grace_hopper.jugador ("userName") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID,
+    CONSTRAINT n_jugadas CHECK (n_jugadas <= (n_ganadas_local + n_ganadas_online)) NOT VALID
 )
 
 TABLESPACE pg_default;
