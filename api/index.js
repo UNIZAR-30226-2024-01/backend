474ea2c44ab53f555cd419bcb8f3c7d071fc77aa
Pablo Moreno <841972@unizar.es>
2024-03-12 13:15:09 +0100

    pruebas

--- api/index.js
+++ api/index.js
@@ -18,7 +18,8 @@ const port = 3000
 const server = createServer(app)
 const io = new Server(server,{
   cors: {
-    origin: "http://localhost:5173",
+    //origin: "http://localhost:5173",
+    origin: "http://localhost:4200",
     // origin: ["http://localhost:5173", "http://localhost:4200"],
   }
 });
