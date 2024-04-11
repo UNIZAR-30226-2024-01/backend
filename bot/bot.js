ca6c09c7faedb77e2910f036e66e89471ccda730
846088romeo <146849583+846088romeo@users.noreply.github.com>
2024-04-11 19:58:20 +0200

    start decision bot lvl 1

--- bot/bot.js
+++ bot/bot.js
@@ -3,13 +3,18 @@ const { spawn } = require('child_process');
 async function botRun(obj) {
     console.log("Bot is running");
     // console.log(obj);
+
+    // Tarjeta de jugador, query de la base de datos
+
+
     
     // Argumentos para pasar al script de Python
     // turn o susp, celda id, dados, nivel
     const args = ['turn', '451', '2', '2'];
+    const args2 = ['susp', 1, 3, 3, 5, 'MR.SOPER', 'CAFETERIA', 'ROUTER', true, tarjeta, 'MR.SOPER'];
     
     // Ejecutar el script de Python
-    const pythonProcess = spawn('python', ['../bot/bot.py', ...args]);
+    const pythonProcess = spawn('python', ['../bot/bot.py', ...args2]);
     
     pythonProcess.stdout.on('data', (data) => {
         console.log(`stdout: ${data}`);
--- bot/bot.py
+++ bot/bot.py
@@ -106,6 +106,38 @@ def turn():
     # Hay que ver cuál es la mejor opción de casilla a la que ir
     # Dependerá de la distancia a la sala y de si llega o no con la tirada actual
 
+def sospecha():
+    initSusp = 1
+    yo = sys.argv[initSusp]
+    lvl = sys.arg[initSusp + 1]
+    turn = sys.arg[initSusp + 2]
+    asker = sys.arg[initSusp + 3]
+    asked = sys.arg[initSusp + 4]
+    who = sys.arg[initSusp + 5]
+    where = sys.arg[initSusp + 6]
+    what = sys.arg[initSusp + 7]
+    hasSmg = sys.arg[initSusp + 8]
+    suspCard = sys.arg[initSusp + 9]
+    card = sys.arg[initSusp + 10]
+
+    # Pasar el string suspCard a una matriz
+    numPlayers = 6
+    numThings = 18
+    sospechas = [[random.choice([0,1,-1]) for _ in range(numPlayers)] for _ in range(numThings)]
+
+    if lvl == 1:
+        if turn == yo:
+            if hasSmg:
+                # printear sospechas
+                print(sospechas)
+            else:
+                pass
+                # suspCarf[][]
+    else:
+        pass
+
+
+    
 
 if __name__ == "__main__":
 
@@ -126,6 +158,7 @@ if __name__ == "__main__":
         # Por hacer aún
         # No sé aún cómo se va a pasar la hoja de sospechas, si es json, parámetro o cómo
         print("Sospecha")
+        sospecha()
 
     time_fin = time.time()
     print("Tiempo de ejecución: ", time_fin - time_ini)
