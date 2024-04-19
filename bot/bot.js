e7b1a057d31748ca62a0e72641852521f749df70
846088romeo <146849583+846088romeo@users.noreply.github.com>
2024-04-20 01:27:39 +0200

    lvl 2 bots + creación con 3 cartas

--- bot/bot.js
+++ bot/bot.js
@@ -2,16 +2,26 @@ const { spawn } = require('child_process');
 const constants = require('../api/constants.js');
 const N_PLAYERS = 6;
 
-function createBot() {
+function createBot(me, where, who, what) {
   console.log('Creating bot...');
 
   // Crear un string de (N_PLACES+N_ROOMS+N_THINGS)*N_PLAYERS
   const tarjeta = [];
   for (let i = 0; i < constants.CHARACTERS_NAMES.length + constants.GUNS_NAMES.length + constants.ROOMS_NAMES.length; i++) {
     for (let j = 0; j < N_PLAYERS; j++) {
-      // Numero aleatorio entre 0 y 100
-      const random = Math.floor(Math.random() * 101);
-      tarjeta.push(random);
+      if (i === where || i === who || i === what) {
+        if(j === me) {
+          tarjeta.push(100);
+        } else {
+          tarjeta.push(0);
+        }
+      } else {
+        if (j === me) {
+          tarjeta.push(0);
+        } else {
+          tarjeta.push(50);
+        }
+      }
     }
   }
 
@@ -45,9 +55,10 @@ async function updateCard(tarjeta){
   console.log('Updating card...');
   console.log('Parameters: <me> <lvl> <asker> <holder> <where> <who> <what> <hasSmg> <card>');
 
-  const args = [3, 1, 3, 1, 'biblioteca', 'miss REDES', 'troyano', 0, tarjeta];
+  const args_lvl1 = [3, 1, 3, 1, 'biblioteca', 'miss REDES', 'troyano', 0, tarjeta];
+  const args_lvl2 = [3, 2, 2, 1, 'recepcion', 'mr SOPER', 'cd', 2, tarjeta];
 
-  const pythonProcess = spawn('python3', ['../bot/updateCard.py', ...args]);
+  const pythonProcess = spawn('python3', ['../bot/updateCard.py', ...args_lvl2]);
 
   pythonProcess.stdout.on('data', (data) => {
     console.log(`stdout: ${data}`);
