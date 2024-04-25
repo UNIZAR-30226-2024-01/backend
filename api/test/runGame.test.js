18d4516400e3fd7dbe77932bafd3df76cc0b64f7
analanga <126185969+analanga@users.noreply.github.com>
2024-04-25 11:10:55 +0200

    NEW

--- api/test/runGame.test.js
+++ api/test/runGame.test.js
@@ -1,35 +1,38 @@
 const { runGame } = require('../game.js');
+const controller = require('../controller.js');
 
+// Mock de controller
+jest.mock('../controller.js', () => ({
+  getPlayersCharacter: jest.fn(() => ({ players: [{ userName: 'testUser', character: null }] })),
+  dealCards: jest.fn(() => ({ cards: [[1, 2, 3]] })), 
+  selectCharacter: jest.fn(),
+}));
+
+const group = 'testGroup';
+
+const mockIo = {
+  sockets: {
+    adapter: {
+      rooms: new Map([[group, new Set(['socket1', 'socket2'])]]), 
+    },
+    sockets: new Map([
+      ['socket1', { handshake: { auth: { username: 'testUser1' } } }],
+      ['socket2', { handshake: { auth: { username: 'testUser2' } } }],
+    ]),
+    get: jest.fn((socketId) => mockIo.sockets.sockets.get(socketId)),
+    emit: jest.fn(), 
+  },
+  to: jest.fn((room) => mockIo.sockets), 
+  emit: jest.fn(),
+};
+
+//REVISAR, NO ESTOY NADA SEGURA
 describe('runGame', () => {
-  test('should start the game and handle turns correctly', async () => {
-    // Mock para io y group
-   /* const io = {
-      sockets: {
-        adapter: {
-          rooms: new Map().set('group', new Set(['socket1', 'socket2']))
-        },
-        sockets: new Map([
-          ['socket1', { emit: jest.fn() }],
-          ['socket2', { emit: jest.fn() }]
-        ])
-      }
-    };
-
-    const group = 'group';
-
-    // Llamar a runGame
-    await runGame(io, group);
-
-    // Verificar que se emitieron eventos start-game a los sockets
-    expect(io.sockets.sockets.get('socket1').emit).toHaveBeenCalledWith('start-game', expect.any(Object));
-    expect(io.sockets.sockets.get('socket2').emit).toHaveBeenCalledWith('start-game', expect.any(Object));
-
-    // Simular el turno de los jugadores
-    const handleTurnoSpy = jest.spyOn(runGameModule, 'handleTurno');
-    handleTurnoSpy.mockImplementationOnce(() => {});
-    handleTurnoSpy.mockImplementationOnce(() => {});
-
-    // Verificar que se llamó a handleTurno para cada jugador
-    expect(handleTurnoSpy).toHaveBeenCalledTimes(2);*/
+  it('should run the game with the given parameters', async () => {
+    const group = 'testGroup';
+    await runGame(mockIo, group);
+    expect(controller.getPlayersCharacter).toHaveBeenCalledWith(group);
+    expect(mockIo.sockets.emit).toHaveBeenCalledWith('start-game', expect.any(Object));
+ 
   });
 });
